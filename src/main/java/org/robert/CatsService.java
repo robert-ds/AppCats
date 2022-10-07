843d1e3049d388081c022d1bfe33bf5cb86e2eda
Robert Vásquez <vasquez.robert.andres@gmail.com>
2022-10-07 15:49:59 -0400

    Createmenu for app and create class CatsService and method viewCats()

---
+++ src/main/java/org/robert/CatsService.java
@@ -0,0 +1,14 @@
+package org.robert;
+
+/**
+ * Created with IntelliJ IDEA
+ * Created By Robert Vásquez
+ * Date: 7/10/22
+ * Time: 3:45 p. m.
+ */
+
+public class CatsService {
+  public static void viewCats(){
+
+  }
+}
--- src/main/java/org/robert/Main.java
+++ src/main/java/org/robert/Main.java
@@ -1,14 +1,48 @@
 package org.robert;
 
+import javax.swing.*;
+
 /**
  * Created with IntelliJ IDEA
  * Created By Robert Vásquez
  * Date: 7/10/22
  * Time: 3:08 p. m.
  */
+
 public class Main {
 
   public static void main(String[] args){
+
+    int opt = -1;
+    String[] botones = {
+        "1. View Cats","2. Exit"
+    };
+
+    do{
+
+      String option =  (String) JOptionPane.showInputDialog(null,"Cats Java","Main Menu", JOptionPane.INFORMATION_MESSAGE, null, botones,botones[0]);
+
+      // Validate option  selected for user
+      for(int i = 0;i < botones.length; i++){
+        if(option.equals(botones[i])){
+          opt = i;
+        }
+      }
+
+      switch(opt){
+        case 0:
+          CatsService.viewCats();
+          break;
+
+        case 1:
+          break;
+
+        default:
+          break;
+      }
+
+    }while(opt != 1);
+
 
   }
 
